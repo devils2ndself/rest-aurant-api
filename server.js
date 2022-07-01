4337457d477a650260a38b50d6e43e7e9a8cd8ac
Artem Tanyhin <magic.cat.775@gmail.com>
2022-06-30 22:03:22 -0400

    env and gitignore

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-/*********************************************************************************
-* WEB422 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: Artem Tanyhin Student ID: 107958209 Date: 22/01/2020
-* Heroku Link: https://shielded-beyond-25498.herokuapp.com/
-*
-********************************************************************************/
-
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const RestaurantDB = require("./modules/restaurantDB.js");
-const db = new RestaurantDB("mongodb+srv://ArtemTanyhin:itsmeArtem@webassignment.gtlfb.mongodb.net/sample_restaurants?retryWrites=true&w=majority");
+require('dotenv').config();
+const db = new RestaurantDB(process.env.MONGO_CON_STR);
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
